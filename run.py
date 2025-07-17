ffeeb323a4e7605b315e32d7db45ef39a1e697b9
Facundo Alvarez Motta <facundoalvarezmotta@gmail.com>
2025-07-17 16:15:02 -0300

    Commiting small modifications on the timesnet running process.

--- run.py
+++ run.py
@@ -26,16 +26,16 @@ if __name__ == '__main__':
         freq='d',  # freq for time features encoding, options:[s:secondly, t:minutely, h:hourly, d:daily, b:business days, w:weekly, m:monthly], you can also use more detailed freq like 15min or 3h
         checkpoints='./checkpoints/',  # location of model checkpoints
 
-        seq_len=30,  # input sequence length
+        seq_len=60,  # input sequence length
         pred_len=1,  # prediction sequence length
         inverse=True,  # inverse output data
 
-        top_k=5,  # for TimesBlock
-        num_kernels=6,  # for Inception
+        top_k=7,  # for TimesBlock
+        num_kernels=5,  # for Inception
         enc_in=9,  # encoder input size
         c_out=1,  # output size
         d_model=512,  # dimension of model
-        e_layers=2,  # num of encoder layers
+        e_layers=10,  # num of encoder layers
         d_ff=256,  # dimension of fcn
         dropout=0.1,  # dropout
         embed='fixed',  # time features encoding, options:[timeF, fixed, learned]
@@ -43,9 +43,9 @@ if __name__ == '__main__':
 
         num_workers=10,  # data loader num workers
         itr=1,  # experiments times
-        train_epochs=10,  # train epochs
+        train_epochs=100,  # train epochs
         batch_size=32,  # batch size of train input data
-        patience=7,  # early stopping patience
+        patience=15,  # early stopping patience
         learning_rate=0.0001,  # optimizer learning rate
         lradj='type1',  # adjust learning rate
         use_amp=False,  # use automatic mixed precision training
